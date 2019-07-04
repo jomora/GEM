5a351006afe5d251b3d958a6d2087e4dd2fd5df1
Jonas Molina Ramirez <jonas.molinaramirez@stud.tu-darmstadt.de>
2019-07-04 23:52:36 +0200

    WIP

--- test.py
+++ test.py
@@ -12,11 +12,13 @@ from gem.embedding.lle      import LocallyLinearEmbedding
 from gem.embedding.node2vec import node2vec
 from gem.embedding.sdne     import SDNE
 import os
-
+import keras.backend.tensorflow_backend as K
 
 # File that contains the edges. Format: source target
 # Optionally, you can add weights as third column: source target weight
-edge_f = os.environ['SEML_DATA']+'/output/all/all.edgelist'
+#edge_f = os.environ['SEML_DATA']+'/output/all/all.edgelist'
+edge_f = os.environ['SEML_DATA']+'/output/all2/all2.edgelist'
+
 # Specify whether the edges are directed
 isDirected = True
 
@@ -26,11 +28,11 @@ G = G.to_directed()
 
 models = []
 # You can comment out the methods you don't want to run
-models.append(("GraphFactorization",GraphFactorization(2, 50000, 1*10**-4, 1.0)))
-models.append(("HOPE",HOPE(4, 0.01)))
-models.append(("LaplacianEigenmaps",LaplacianEigenmaps(2)))
-models.append(("LocallyLinearEmbedding",LocallyLinearEmbedding(2)))
-models.append(("node2vec",node2vec(2, 1, 80, 10, 10, 1, 1)))
+#models.append(("GraphFactorization",GraphFactorization(2, 50000, 1*10**-4, 1.0)))
+#models.append(("HOPE",HOPE(4, 0.01)))
+#models.append(("LaplacianEigenmaps",LaplacianEigenmaps(2)))
+#models.append(("LocallyLinearEmbedding",LocallyLinearEmbedding(2)))
+#models.append(("node2vec",node2vec(2, 1, 80, 10, 10, 1, 1)))
 models.append(("SDNE",SDNE(d=2, beta=5, alpha=1e-5, nu1=1e-6, nu2=1e-6, K=3,n_units=[50, 15,], rho=0.3, n_iter=50, xeta=0.01,n_batch=500,
                 modelfile=['./intermediate/enc_model.json', './intermediate/dec_model.json'],
                 weightfile=['./intermediate/enc_weights.hdf5', './intermediate/dec_weights.hdf5'])))
@@ -43,10 +45,12 @@ for (name,embedding) in models:
     print ('Num nodes: %d, num edges: %d' % (G.number_of_nodes(), G.number_of_edges()))
     t1 = time()
     # Learn embedding - accepts a networkx graph or file with edge list
-    Y, t = embedding.learn_embedding(graph=G, edge_f=None, is_weighted=True, no_python=True)
-    print (embedding._method_name+':\n\tTraining time: %f' % (time() - t1))
-    # Evaluate on graph reconstruction
-    MAP, prec_curv = gr.evaluateStaticGraphReconstruction(G, embedding, Y, None)
+    with K.tf.device('/gpu:3'):
+        K.get_session()#K.tf.Session(config=K.tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)))
+        Y, t = embedding.learn_embedding(graph=G, edge_f=None, is_weighted=True, no_python=True)
+        print (embedding._method_name+':\n\tTraining time: %f' % (time() - t1))
+        # Evaluate on graph reconstruction
+        MAP, prec_curv = gr.evaluateStaticGraphReconstruction(G, embedding, Y, None)
     #---------------------------------------------------------------------------------
     print(("\tMAP: {} \t preccision curve: {}\n\n\n\n"+'-'*100).format(MAP,prec_curv))
     #---------------------------------------------------------------------------------
